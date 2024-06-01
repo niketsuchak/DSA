0fbd4c8e6b7d1d6313ba2ff91807570082e17e06
niketsuchak <93976932+niketsuchak@users.noreply.github.com>
2024-06-01 21:19:57 +0530

    Create pascalstriangle.cpp

---
+++ pascalstriangle.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    vector<int> rowGenerator(int numRows){
+        long long ans = 1;
+        vector<int> ansRow;
+        ansRow.push_back(1);
+        for(int col=1; col<numRows; col++){
+            ans = ans * (numRows-col);
+            ans = ans / (col);
+            ansRow.push_back(ans);
+        }
+        return ansRow;
+    }
+    vector<vector<int>> generate(int numRows) {
+        
+        vector<vector<int>> ans;
+        for(int i=1; i<=numRows; i++){
+            ans.push_back(rowGenerator(i));
+        }
+
+        return ans;
+
+    
+    }
+};
